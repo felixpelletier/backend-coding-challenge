eaa7446486e45603debdab500cec4e680bb4bd88
Félix Pelletier <felixpelletier23@gmail.com>
2019-05-21 20:15:29 -0400

    Added a factory for the main environment

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import main_factory
 from src.web import flask_app_constructor
 
+city_suggestions = main_factory.create_application()
+flask_app = flask_app_constructor.construct_app(city_suggestions)
+
 
 # noinspection Pylint
 def main():
-
-    city_suggestions = main_factory.create_application()
-    flask_app = flask_app_constructor.construct_app(city_suggestions)
     flask_app.run()
 
 
---
+++ main_factory.py
@@ -0,0 +1,21 @@
+
+from src.suggestions.city_infos.geoname_gazetter_city_infos_provider import GeonameGazetterFileCityInfoProvider
+from src.suggestions.service import CitySuggestionsService
+from src.suggestions.domain import city_scorer
+from src.suggestions.domain import metrics
+
+
+GEONAME_GAZETTER_FILE_PATH = "data/cities_canada-usa.tsv"
+
+
+def create_application():
+    # There should be a factory here somewhere
+    city_infos_provider = GeonameGazetterFileCityInfoProvider(GEONAME_GAZETTER_FILE_PATH)
+
+    city_scorer_implementation = city_scorer.CityScorer()
+    city_scorer_implementation.add_metric(metrics.CityNameStartsWithSimilarityMetric(), 10)
+    city_scorer_implementation.add_metric(metrics.RatcliffObershelpCityNameSimilarityMetric(), 5)
+    city_scorer_implementation.add_metric(metrics.HaversineLocationDistanceMetric(), 2)
+
+    city_suggestions = CitySuggestionsService(city_infos_provider, city_scorer_implementation)
+
